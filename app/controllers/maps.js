909f6cf8db1b2dddd7cda36917568b77de415c22
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-11-08 20:45:39 +0300

    map sizing to fit page
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- app/controllers/maps.js
+++ app/controllers/maps.js
@@ -1,11 +1,20 @@
 import Controller from '@ember/controller';
 
 export default class MapsController extends Controller {
-  lat = 45.528298;
-  lng = -122.662986;
-  zoom = 14;
+  lat = -1.286389;
+  lng = 36.817223;
+  zoom = 6;
 
-  emberConfLocation = [45.528298, -122.662986];
+  emberConfLocation = [3.0947667, 35.6155151];
+  emberConfLocationData =
+    'The Oregon Convention Center 777 NE Martin Luther King Jr Blvd Portland, OR 97232';
 
-  hotel = [45.530891, -122.655504];
+  full = [
+    [3.0947667, 35.5155151],
+    [3.0948866, 35.3155183],
+    [3.0946566, 35.4155232],
+    [3.0948979, 35.7156474],
+    [3.120818, 35.6046763],
+    [3.0947407, 35.7156056],
+  ];
 }
