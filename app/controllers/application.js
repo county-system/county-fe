4b958b12c7397f76ea99d947994ca72f86e74ded
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-11-27 02:25:34 +0300

    main page
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,5 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
-
-
 export default class ApplicationController extends Controller {
   @service router;
   @service me;
@@ -10,19 +7,4 @@ export default class ApplicationController extends Controller {
   get route() {
     return this.router.currentRouteName;
   }
-
-  // @action
-  // bgImage() {
-  //   if (this.router.currentRouteName === 'index'){
-  //     const banner = document.querySelector('.page-wrapper');
-  //     window.addEventListener('DOMContentLoaded', () => {
-  //       banner.classList.add('bg-image');
-  //     });
-  //   } else {
-  //     const banner = document.querySelector('.page-wrapper');
-  //     window.addEventListener('DOMContentLoaded', () => {
-  //       banner.classList.remove('bg-image');
-  //     });
-  //   }
-  // }
 }
