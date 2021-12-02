763667a46204d71b2df806a633c655768a818274
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-12-02 22:00:42 +0300

    user data loaded
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

---
+++ app/controllers/users.js
@@ -0,0 +1,39 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import LoginValidations from '../validations/login';
+
+class UserForm {
+  @tracked email;
+  @tracked firstname;
+  @tracked lastname;
+  @tracked password;
+}
+
+export default class UsersController extends Controller {
+  LoginValidations = LoginValidations;
+  @tracked modelData = this.model;
+  @tracked modal1 = false;
+
+  constructor() {
+    super(...arguments);
+    this.userLoginForm = new UserForm();
+  }
+
+  get modelData() {
+    return this.model;
+  }
+
+  @action
+  createUser(model) {
+    let fields = {
+      username: model.username,
+      email: model.email,
+      contactNumber: model.contactNumber,
+      password: model.password,
+      inviteCode: model.inviteCode,
+    };
+    console.log(fields);
+    alert('Modal submitted!');
+  }
+}
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
 
-export default class UsersRoute extends Route {}
+export default class UsersRoute extends Route {
+  async model() {
+    // return this.store.findAll('users');
+    const response = await fetch('/api/v2/users.json');
+    return await response.json();
+  }
+}
