57a07877b2f32987381ebf23275b0a9ef87f02ed
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2022-01-12 14:14:45 +0300

    bsbutton for download
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- app/controllers/projects/ward/index.js
+++ app/controllers/projects/ward/index.js
@@ -1,6 +1,23 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import RSVP from 'rsvp';
+import { later } from '@ember/runloop';
 
 export default class ProjectsWardIndexController extends Controller {
   @tracked modelData = this.model;
+
+  /**
+   *
+   * @param {*} format Document format
+   * @returns a downloadable document
+   */
+  @action
+  download(format) {
+    this.model.download(format);
+    console.log(format);
+    return new RSVP.Promise(function (resolve) {
+      later(resolve, 3000);
+    });
+  }
 }
