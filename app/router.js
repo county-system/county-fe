53003cfa1d8d62e0bc73224b0f15a6f869078932
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2022-01-24 13:04:12 +0300

    projects cleanup
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- app/router.js
+++ app/router.js
@@ -18,7 +18,6 @@ Router.map(function () {
   this.route('users');
   this.route('dashboard');
   this.route('maps');
-  this.route('kpi');
   this.route('login');
   this.route('dash');
   this.route('reports');
