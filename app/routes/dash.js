7f5a4f140e0d69239ce5ba1d2bba9b68c90c989a
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2022-01-31 22:16:04 +0300

    home page access
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- app/routes/dash.js
+++ app/routes/dash.js
@@ -1,11 +1,3 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
-export default class DashRoute extends Route {
-  @service me;
-  @service session;
-
-  async beforeModel(transition) {
-    this.session.requireAuthentication(transition, 'login');
-  }
-}
+export default class DashRoute extends Route {}
