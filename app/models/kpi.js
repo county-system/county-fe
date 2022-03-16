92c967bb603164ab6e7f16f2605da557489f9572
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2022-03-16 17:42:34 +0300

    decoupled hardcoded api data
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- app/models/kpi.js
+++ app/models/kpi.js
@@ -1,14 +1,23 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class KpiModel extends Model {
-  @attr project_name;
-  @attr implementing_entity;
-  @attr sub_county;
-  @attr ward;
-  @attr project_status;
-  @attr project_cost;
+  @attr department;
+  @attr indicators;
+  @attr unit;
+  @attr baseline_year;
+  @attr baseline_value;
+  @attr financial_year;
+  @attr actual_achievement;
 
   @attr('date') start_date;
   @attr('date') end_date;
   @attr('date') completion_date;
+
+  get financial_year_data() {
+    return this.financial_year.year;
+  }
+
+  get financial_year_number() {
+    return this.financial_year.number;
+  }
 }
