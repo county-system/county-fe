cc861f118f56ec1a889d5ce2d02a4ec9c96af97f
mosesokemwa <moses@lami.world>
2022-03-17 12:48:21 +0300

    clean data
    Signed-off-by: mosesokemwa <moses@lami.world>

--- app/routes/dash.js
+++ app/routes/dash.js
@@ -8,4 +8,10 @@ export default class DashRoute extends Route {
   async beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
   }
+
+  async model() {
+    // return await this.store.findAll('kpi', { reload: true });
+    const response = await fetch('/api/v2/kpi.json');
+    return await response.json();
+  }
 }
